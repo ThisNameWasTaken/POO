04639223019276b20a1f87f86eefa4bec5c965cc
Adrian <adriandinca2008@yahoo.com>
2018-04-28 17:00:41 +0300

    feat: Add basic movement

--- SurvivalGame/InputController.cpp
+++ SurvivalGame/InputController.cpp
@@ -1,6 +1,8 @@
 #include "InputController.h"
 #include "Vector2D.h"
 #include "Character.h"
+#include <windows.h>
+#include "Keyboard.h"
 
 InputController::InputController() {
 
@@ -20,5 +22,24 @@ void InputController::onUpdate() {
 	if (!this->character) {
 		return;
 	}
-	
+
+	if (GetAsyncKeyState(Keyboard::W)) {
+		// Move upwards
+		this->character->move(Vector2D(0, -1));
+	}
+
+	if (GetAsyncKeyState(Keyboard::A)) {
+		// Move to the left
+		this->character->move(Vector2D(-1, 0));
+	}
+
+	if (GetAsyncKeyState(Keyboard::S)) {
+		// Move downwards
+		this->character->move(Vector2D(0, 1));
+	}
+
+	if (GetAsyncKeyState(Keyboard::D)) {
+		// Move to the right
+		this->character->move(Vector2D(1, 0));
+	}
 }
