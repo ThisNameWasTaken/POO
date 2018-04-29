72bd6d3849785411c59f1fc004ce1f593f893820
Adrian <adriandinca2008@yahoo.com>
2018-04-29 22:32:20 +0300

    feat: Add basic combat system

--- SurvivalGame/Agent.cpp
+++ SurvivalGame/Agent.cpp
@@ -1,17 +1,80 @@
 #include "Agent.h"
+#include "Controller.h"
+#include "Console.h"
+#include "Random.h"
+#include "Scene.h"
+#include <string>
 
-Agent::Agent(const Vector2D& Position, const string& tagName) : Character(Position) {
-
+Agent::Agent(const Vector2D& position, const string& tagName) : Character(position) {
+	this->tagName = tagName;
 }
 
 Agent::Agent(const Agent& agent) : Character(agent) {
 	this->tagName = agent.getTagName();
 }
 
-Agent::Agent(const Sprite& sprite, Controller* controller, const string& tagName) : Character(sprite, controller) {
+Agent::Agent(const Sprite& sprite, Controller* controller, const Vector2D& position, const string& tagName) : Character(sprite, controller, position) {
 	this->tagName = tagName;
 }
 
 Agent::~Agent() {
 
+}
+
+void Agent::beginPlay() {
+	Super::beginPlay();
+	const Scene* scene = Scene::getActiveScene();
+	if (scene) {
+		this->setPosition(Vector2D(random(scene->getHeight()), random(scene->getWidth())));
+	}
+}
+
+void Agent::update() {
+	Super::update();
+
+	Console::setCursorPosition(0, 56);
+	cout << "Actors count: " << Actor::getWorldActors().size() << "\n";
+
+	set<Actor*> overlappingActors = this->getOverlappingActors();
+	for (Actor* actor : overlappingActors) {
+		Agent* agent = dynamic_cast<Agent*>(actor);
+		if (agent) {
+			this->damage(agent);
+
+			if (this->getHP() > 0) {
+				cout << this->getTagName() << " eliminated " << agent->getTagName() << "\n";
+				agent->Destroy();
+			}
+			else if (agent->getHP() > 0) {
+				cout << agent->getTagName() << " eliminated " << this->getTagName() << "\n";
+				this->Destroy();
+			}
+			else {
+				cout << this->getTagName() << " and " << agent->getTagName() << " eliminated each other\n";
+				this->Destroy();
+				agent->Destroy();
+			}
+		}
+	}
+}
+
+void Agent::damage(Agent* target) {
+	if (target->getHP() <= 0) {
+		return;
+	}
+	// The amout of damage which remains to be dealt
+	// after the target's armor is broken
+	int damageToBeDealt = this->getAD();
+
+	int targetArmor = target->getArmor();
+	target->setArmor(targetArmor - damageToBeDealt);
+
+	damageToBeDealt -= targetArmor;
+	target->setHP(target->getHP() - damageToBeDealt);
+
+	target->onDamageTaken(this);
+}
+
+void Agent::onDamageTaken(Agent* instigator) {
+	this->damage(instigator);
 }
--- SurvivalGame/Agent.h
+++ SurvivalGame/Agent.h
@@ -1,17 +1,49 @@
 #pragma once
 #include "Character.h"
+#include <math.h>
 
 class Agent : public Character {
 public:
 	Agent(const Vector2D& position = Vector2D(0, 0), const string& tagName = "");
 	Agent(const Agent& agent);
-	Agent(const Sprite& sprite, class Controller* controller = nullptr, const string& tagName = "");
+	Agent(const Sprite& sprite, class Controller* controller = nullptr, const Vector2D& position = Vector2D(0, 0), const string& tagName = "");
 	~Agent();
+
+	virtual void beginPlay() override;
+	virtual void update() override;
 
 	inline string getTagName() const { return this->tagName; }
 	inline void setTagName(const string& tagName) { this->tagName = tagName; }
 
+	inline int getHP() const { return this->HP; }
+	inline void setHP(int HP) { if (HP > maxHP) { this->HP = maxHP; } else { this->HP = HP; } }
+
+	inline int getArmor() const { return this->armor; }
+	inline void setArmor(int armor) { if (armor > maxArmor) { this->armor = maxArmor; } else { this->armor = armor; } }
+
+	inline int getAD() const { return this->AD; }
+	inline void setAD(int AD) { if (AD < 0) { this->AD = 0; } else { this->AD = AD; } }
+
+	/**
+	 * Damages a target Agent by this instance's attack damage (AD)
+	 * @param target - the Agent to be damaged
+	 */
+	virtual void damage(Agent* target);
+
+	/**
+	 * Called when an Agent is attacked
+	 * @param instigator - the Agent which attacks this instance
+	 */
+	virtual void onDamageTaken(Agent* instigator);
+
 protected:
+	int maxHP;
+	int maxArmor;
+
+private:
 	string tagName;
+	int HP;
+	int armor;
+	int AD;
 };
 
