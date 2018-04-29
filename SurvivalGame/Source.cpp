ce57069d89f7edcc03a6f5f7e18ca03200819710
Adrian <adriandinca2008@yahoo.com>
2018-04-29 23:57:21 +0300

    chore: Add source code

--- SurvivalGame/Source.cpp
+++ SurvivalGame/Source.cpp
@@ -1,35 +1,48 @@
 #include "Engine.h"
-#include <conio.h>
+#include "Kitty.h"
+#include "Dog.h"
+#include "Cat.h"
+#include "Ghost.h"
+#include "HealthPack.h"
+#include "Armor.h"
+#include "SharpenedClaws.h"
 
 int main() {
 	Engine::start();
 
 	Texture tileTexture("Textures/tile.txt");
-	Texture catTexture("Textures/cat_2.txt");
-	Texture dogTexture("Textures/dog.txt");
-	Texture owlTexture("Textures/owl.txt");
-	Texture ghostTexture("Textures/ghost.txt");
-	Texture brickTexture("Textures/brick.txt");
-
 	Sprite tileSprite(&tileTexture);
-	Sprite catSprite(&catTexture);
-	Sprite dogSprite(&dogTexture);
-	Sprite owlSprite(&owlTexture);
-	Sprite ghostSprite(&ghostTexture);
-	Sprite brickSprite(&brickTexture);
 
 	InputController playerController;
-	Character player(catSprite, &playerController);
+	Ghost player(&playerController);
+
+	AIController ghostController;
+	Ghost ghost(&ghostController);
+
+	AIController catController;
+	Cat cat(&catController);
+
+	AIController catController2;
+	Cat cat2(&catController2);
+
+	AIController dogController;
+	Dog dog(&dogController);
+
+	AIController dogController2;
+	Dog dog2(&dogController2);
+
+	AIController kittyController;
+	Kitty kitty(&kittyController);
+
+	SharpenedClaws claws[4];
+	HealthPack healthPacks[4];
+	Armor armors[4];
 	
-	Scene scene(10, 10, tileSprite, Actor::getWorldActors());
+	Scene scene(19, 11, tileSprite, Actor::getWorldActors());
 	while (true) {
 		scene.render();
-		cout << "Waiting on the other players...";
-		Sleep(1000);
+		Sleep(700);
 	}
-
-	// cout << catSprite << " " << dogSprite << "  " << catSprite << " " << owlSprite << " " << ghostSprite << " "  << endl << endl << endl << endl << endl << endl;
-	// cout << endl << endl << endl << endl << endl;
 
 	system("pause");
 	return 0;
