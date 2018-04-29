0544f22c0e7707677dbaa6ab54bc952dc4d7dcc8
Adrian <adriandinca2008@yahoo.com>
2018-04-29 22:31:35 +0300

    chore: Update the kitty class

--- SurvivalGame/Kitty.cpp
+++ SurvivalGame/Kitty.cpp
@@ -7,7 +7,7 @@ Kitty::Kitty(const Vector2D& position, const string& tagName) : Agent(position) 
 
 }
 
-Kitty::Kitty(Controller* controller, const Vector2D& position, const string& tagName) : Agent(Sprite(&texture), controller, tagName) {
+Kitty::Kitty(Controller* controller, const Vector2D& position, const string& tagName) : Agent(Sprite(&texture), controller, position, tagName) {
 
 }
 
@@ -16,10 +16,15 @@ Kitty::~Kitty() {
 }
 
 void Kitty::beginPlay() {
-	Super::beginPlay();
-	this->setPosition(Vector2D(5, 5));
+	Agent::beginPlay();
+
+	this->maxHP = 5000;
+	this->maxArmor = 50;
+	this->setHP(5000);
+	this->setArmor(0);
+	this->setAD(1550);
 }
 
 void Kitty::update() {
-	Super::update();
+	Agent::update();
 }
