7bca10d7a979dff9e54ec29994481e0799b909df
Adrian <adriandinca2008@yahoo.com>
2018-04-30 19:20:10 +0300

    feat: Improve AI

--- SurvivalGame/AIController.cpp
+++ SurvivalGame/AIController.cpp
@@ -22,41 +22,56 @@ void AIController::onUpdate() {
 		return;
 	}
 
-	switch (random(8)) {
-	case 0:
-		// Move upwards
-		this->character->move(Vector2D(0, -1));
-		break;
-	case 1:
-		// Move to the left
-		this->character->move(Vector2D(-1, 0));
-		break;
-	case 2:
-		// Move downwards
-		this->character->move(Vector2D(0, 1));
-		break;
-	case 3:
-		// Move to the right
-		this->character->move(Vector2D(1, 0));
-		break;
-	case 4:
-		// Move to the top-left
-		this->character->move(Vector2D(-1, -1));
-		break;
-	case 5:
-		// Move to the bottom-left
-		this->character->move(Vector2D(-1, 1));
-		break;
-	case 6:
-		// Move to the bottom-right
-		this->character->move(Vector2D(1, 1));
-		break;
-	case 7:
-		// Move to the top-right
-		this->character->move(Vector2D(1, -1));
-		break;
-	default:
-		// Idle
-		break;
+	const Actor* closestActor = nullptr;
+	// The closest position between the controlled Character and another Actor
+	Vector2D closestDeltaPosition(INT_MAX, INT_MAX);
+	Vector2D deltaPosition; 
+
+	const set<Actor*> actors = this->character->getWorldActors();
+	for (const Actor* actor : actors) {
+		if (!actor) {
+			continue;
+		}
+
+		// The Actor should not be the controlled Character
+		if (actor == this->character) {
+			continue;
+		}
+
+		// Check if this Actor is closers
+		deltaPosition = this->character->getPosition() - actor->getPosition();
+		deltaPosition.x = abs(deltaPosition.x);
+		deltaPosition.y = abs(deltaPosition.y);
+		if (deltaPosition.x < closestDeltaPosition.x && deltaPosition.y < closestDeltaPosition.y) {
+			closestActor = actor;
+			closestDeltaPosition = deltaPosition;
+		}
 	}
+
+	if (closestActor) {
+		chase(closestActor);
+	}
+}
+
+void AIController::chase(const Actor* actor) {
+	const Vector2D& actorPosition = actor->getPosition();
+	Vector2D newPosition = character->getPosition();
+	
+	if (newPosition.x < actorPosition.x) { // If the chased Actor is on the right side
+		// Go to the right
+		newPosition.x++;
+	} else if (newPosition.x > actorPosition.x) { // If the chased Actor is on the left side
+		// Go to the left
+		newPosition.x--;
+	}
+
+	if (newPosition.y < actorPosition.y) { // If the chased Actor is on the top side
+		// Go higher
+		newPosition.y++;
+	} else if (newPosition.y > actorPosition.y) { // If the chased Actor is on the bottom side
+		// Go lower
+		newPosition.y--;
+	}
+
+	this->character->setPosition(newPosition);
 }
--- SurvivalGame/AIController.h
+++ SurvivalGame/AIController.h
@@ -10,6 +10,10 @@ public:
 	virtual void onBeginPlay() override;
 	virtual void onUpdate() override;
 
-	
+	/**
+	 * Chases a given Actor until it has been reached
+	 * @param actor - the Actor to chase
+	 */
+	virtual void chase(const class Actor* actor);
 };
 
